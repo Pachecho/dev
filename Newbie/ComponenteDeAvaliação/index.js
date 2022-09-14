337414705b6125c7b3d4f34bf5b3bf54a34635ea
Diogo Pacheco <diogopachecopb269@gmail.com>
2022-09-14 16:49:30 -0300

    Alterações Projeto

---
+++ Newbie/ComponenteDeAvaliação/index.js
@@ -0,0 +1,9 @@
+function trocarCor(element, color) {
+    element.style.backgroundColor = color;
+    document.querySelectorAll.remove("color")
+    element.setAttribute("color") 
+
+    
+    
+
+}
